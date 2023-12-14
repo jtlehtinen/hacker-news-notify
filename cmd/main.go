38ada326667d0947538079d6bc995f364c246136
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-14 23:08:18 +0200

    wire up notification settings to application data structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,59 @@
 package main
 
+import "sync"
+
 type application struct {
+	notifyTop  bool
+	notifyBest bool
+	notifyNew  bool
+	mu         sync.Mutex // Guards everything declared before
+
 	top  []int64
 	best []int64
 	new  []int64
 }
 
+func (app *application) getNotifyTop() bool {
+	app.mu.Lock()
+	defer app.mu.Unlock()
+	return app.notifyTop
+}
+
+func (app *application) setNotifyTop(value bool) {
+	app.mu.Lock()
+	app.notifyTop = value
+	app.mu.Unlock()
+}
+
+func (app *application) getNotifyBest() bool {
+	app.mu.Lock()
+	defer app.mu.Unlock()
+	return app.notifyBest
+}
+
+func (app *application) setNotifyBest(value bool) {
+	app.mu.Lock()
+	app.notifyBest = value
+	app.mu.Unlock()
+}
+
+func (app *application) getNotifyNew() bool {
+	app.mu.Lock()
+	defer app.mu.Unlock()
+	return app.notifyNew
+}
+
+func (app *application) setNotifyNew(value bool) {
+	app.mu.Lock()
+	app.notifyNew = value
+	app.mu.Unlock()
+}
+
 func main() {
-	run()
+	app := &application{
+		notifyTop:  true,
+		notifyBest: true,
+		notifyNew:  true,
+	}
+	app.run()
 }
--- cmd/systray.go
+++ cmd/systray.go
@@ -4,7 +4,7 @@ import (
 	"github.com/getlantern/systray"
 )
 
-func onReady() {
+func (app *application) onReady() {
 	toggle := func(item *systray.MenuItem) {
 		if item.Checked() {
 			item.Uncheck()
@@ -17,9 +17,9 @@ func onReady() {
 	systray.SetTitle("Hacker News Notify")
 	systray.SetTooltip("Hacker News Notify")
 
-	top := systray.AddMenuItemCheckbox("Top", "Top", true)
-	best := systray.AddMenuItemCheckbox("Best", "Best", true)
-	new := systray.AddMenuItemCheckbox("New", "New", true)
+	top := systray.AddMenuItemCheckbox("Top", "Top", app.getNotifyTop())
+	best := systray.AddMenuItemCheckbox("Best", "Best", app.getNotifyBest())
+	new := systray.AddMenuItemCheckbox("New", "New", app.getNotifyNew())
 
 	systray.AddSeparator()
 	quit := systray.AddMenuItem("Exit", "Exit")
@@ -29,10 +29,13 @@ func onReady() {
 			select {
 			case <-top.ClickedCh:
 				toggle(top)
+				app.setNotifyTop(top.Checked())
 			case <-best.ClickedCh:
-				toggle(new)
+				toggle(best)
+				app.setNotifyNew(best.Checked())
 			case <-new.ClickedCh:
 				toggle(new)
+				app.setNotifyNew(new.Checked())
 			case <-quit.ClickedCh:
 				systray.Quit()
 				return
@@ -41,7 +44,10 @@ func onReady() {
 	}()
 }
 
-func run() {
-	onExit := func() {}
-	systray.Run(onReady, onExit)
+func (app *application) run() {
+	systray.Run(app.onReady, app.onExit)
+}
+
+func (app *application) onExit() {
+
 }
